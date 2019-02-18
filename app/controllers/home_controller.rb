c0160df2ef135e67de8caee8037d3f47b3c30623
Jan Grodowski <jgrodowski@gmail.com>
2019-02-18 20:57:54 +0000

    redirect to grodowski/undercover

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  def index
+    redirect_to "https://github.com/grodowski/undercover" if Rails.env.production?
+  end
 end
