8b2eed4dffc67da3899f234f31c60a0d861b62fa
Jan Grodowski <jgrodowski@gmail.com>
2019-07-28 13:05:09 +0200

    fix migration rename fail

--- db/migrate/20190420094203_rename_coverage_report_jobs_to_coverage_checks.rb
+++ db/migrate/20190420094203_rename_coverage_report_jobs_to_coverage_checks.rb
@@ -1,5 +1,5 @@
 class RenameCoverageReportJobsToCoverageChecks < ActiveRecord::Migration[5.2]
   def change
-    rename_table :coverage_checks, :coverage_checks
+    rename_table :coverage_report_jobs, :coverage_checks
   end
 end
--- db/migrate/20190420193727_add_annotations_and_base_sha_to_coverage_checks.rb
+++ db/migrate/20190420193727_add_annotations_and_base_sha_to_coverage_checks.rb
@@ -2,6 +2,6 @@ class AddAnnotationsAndBaseShaToCoverageChecks < ActiveRecord::Migration[5.2]
   def change
     add_column :coverage_checks, :annotations, :jsonb
     add_column :coverage_checks, :base_sha, :string
-    rename_column :coverage_checks, :head_sha, :head_sha
+    rename_column :coverage_checks, :commit_sha, :head_sha
   end
 end
