c7d51a40896dc45c145d1819f611e0c12eeab0ca
Jan Grodowski <jgrodowski@gmail.com>
2019-08-15 21:51:45 +0200

    specs for CreateCheckRunJob

--- spec/jobs/create_check_run_job_spec.rb
+++ spec/jobs/create_check_run_job_spec.rb
@@ -3,5 +3,18 @@
 require "rails_helper"
 
 describe CreateCheckRunJob do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "posts a GitHub request with coverage check data" do
+    check = CoverageCheck.create!(
+      head_sha: "953a804",
+      installation_id: "123123",
+      repo: {"full_name" => "author/repo", "default_branch" => "master"}
+    )
+
+    expect(CheckRuns::Create).to receive(:new) do |check_run|
+      expect(check_run.sha).to eq(check.head_sha)
+      expect(check_run.full_name).to eq(check.repo_full_name)
+    end.and_return(instance_double(CheckRuns::Create, post: true))
+
+    described_class.perform_now(check.id)
+  end
 end
