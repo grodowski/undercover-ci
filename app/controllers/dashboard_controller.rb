dad759737f2294888ed0c9552fcf51ea5100d823
Jan Grodowski <jgrodowski@gmail.com>
2019-10-24 21:40:54 +0200

    add app links

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,4 +2,10 @@
 
 class DashboardController < ApplicationController
   before_action :check_current_user
+
+  def dash_installation_url
+    app_name = Rails.env.development? ? "undercover-ci-dev" : "undercoverci"
+    "https://github.com/apps/#{app_name}/installations/new"
+  end
+  helper_method :dash_installation_url
 end
