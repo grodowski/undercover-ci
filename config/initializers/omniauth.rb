670492a9d81794d051ceed5234a89ca9ead1282b
Jan Grodowski <jgrodowski@gmail.com>
2019-10-27 15:00:16 +0100

    ignore state to allow github app post-install callback

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,8 @@
 OmniAuth.config.allowed_request_methods = [:post]
 
 Rails.configuration.middleware.use(OmniAuth::Builder) do
-  provider(:github, ENV["GITHUB_AUTH_KEY"], ENV["GITHUB_AUTH_SECRET"], scope: "user:email")
+  provider(
+    :github, ENV["GITHUB_AUTH_KEY"], ENV["GITHUB_AUTH_SECRET"],
+    scope: "user:email", provider_ignores_state: true
+  )
 end
