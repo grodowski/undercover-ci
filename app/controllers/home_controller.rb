f9d22a4230d2420ade495230802e1d30acdddee7
Jan Grodowski <jgrodowski@gmail.com>
2019-10-27 14:40:42 +0100

    add beta page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,8 @@ class HomeController < ApplicationController
   before_action do
     redirect_to "https://github.com/grodowski/undercover" unless ENV["FF_MARKETING_PAGE"]
   end
+
+  def beta
+    redirect_to root_url if current_user || params[:x] != ENV.fetch("BETA_CODE")
+  end
 end
