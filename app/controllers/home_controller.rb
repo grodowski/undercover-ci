12eade080aba5474afc9487374ed0bc749b9faf7
Jan Grodowski <jgrodowski@gmail.com>
2019-12-16 22:49:49 +0100

    deprecate FF_MARKETING_PAGE

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,6 @@
 class HomeController < ApplicationController
   layout "static_page"
 
-  before_action do
-    redirect_to "https://github.com/grodowski/undercover" unless ENV["FF_MARKETING_PAGE"]
-  end
-
   def beta
     if current_user || params[:x] != ENV.fetch("BETA_CODE")
       redirect_to root_url
