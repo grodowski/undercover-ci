cff69853ed146adb67d6e43c655a8b5ce45ab5d7
Jan Grodowski <jgrodowski@gmail.com>
2020-03-15 20:06:14 +0100

    add specs

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -33,15 +33,61 @@ describe "Dashboard spec" do
     it "redirects to root url for an anonymous user" do
       expect(get("/app")).to redirect_to root_url
     end
+
+    it "renders checks/index when setup is complete" do
+      inst = Installation.create!(installation_id: 43_009_808, user: user)
+      check = CoverageCheck.create!(head_sha: "1337SHA", installation: inst)
+      check.nodes.create(
+        path: "foo.rb",
+        node_name: "hello",
+        node_type: "instance method",
+        start_line: 1,
+        end_line: 3,
+        coverage: 0.6,
+        flagged: true
+      )
+
+      get("/auth/github/callback")
+      expect(get("/app")).to render_template("dashboard/checks/index")
+      expect(response.body).to include(
+        "<a href=\"https://github.com//commit/1337SHA\"><code>1337SHA</code> 👉 <code></code></a>"
+      )
+      expect(response.body).to include("<span class=\"badge badge-warning\">1 warning</span>")
+    end
   end
 
   describe "GET /checks/:id" do
     it "renders checks/show" do
       inst = Installation.create!(installation_id: 43_009_808, user: user)
       check = CoverageCheck.create!(head_sha: "1337SHA", installation: inst)
+      [[1.0, false], [0.8, false], [0.6, true]].each do |coverage, flagged|
+        check.nodes.create!(
+          path: "foo.rb",
+          node_name: "hello",
+          node_type: "instance method",
+          start_line: 1,
+          end_line: 3,
+          coverage: coverage,
+          flagged: flagged
+        )
+      end
 
       get("/auth/github/callback")
       expect(get("/checks/#{check.id}")).to render_template("dashboard/checks/show")
+      expect(response.body).to include("<span class=\"badge badge-warning\">warning</span>")
+      expect(response.body).to include(
+        "Instance method <code>hello</code>. " \
+        "Coverage: <b><span class=\"node-coverage-yellow\">60.0%</span><span>, some changed " \
+        "lines were untested</span></b>"
+      )
+      expect(response.body).to include(
+        "Instance method <code>hello</code>. " \
+        "Coverage: <b><span class=\"node-coverage-default\">100.0%</span><span></span></b>"
+      )
+      expect(response.body).to include(
+        "Instance method <code>hello</code>. " \
+        "Coverage: <b><span class=\"node-coverage-blue\">80.0%</span><span></span></b>"
+      )
     end
   end
 end
