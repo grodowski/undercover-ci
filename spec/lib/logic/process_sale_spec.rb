8338fd75b3464acde0a2965627e07002231c7703
Jan Grodowski <jgrodowski@gmail.com>
2020-10-31 19:50:28 +0100

    update success redirect

--- spec/lib/logic/process_sale_spec.rb
+++ spec/lib/logic/process_sale_spec.rb
@@ -3,6 +3,11 @@
 require "rails_helper"
 
 describe Logic::ProcessSale do
+  before do
+    allow(ENV).to receive(:[]).and_call_original
+    allow(ENV).to receive(:[]).with("FF_SUBSCRIPTION") { "1" }
+  end
+
   let(:gumroad_ping_params) do
     {
       "seller_id" => "xxxx",
