69f5fbb28b278b83d085c7a14f028abe36a99dcb
Jan Grodowski <jgrodowski@gmail.com>
2021-03-27 19:36:46 +0000

    fix sassc segfault?

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+Rails.application.config.assets.configure do |env|
+  env.export_concurrent = false
+end
+
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
