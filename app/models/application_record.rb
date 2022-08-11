6c4717fa50a1b566d8ec5f8f3c9718780d8b3d16
Jan Grodowski <jgrodowski@gmail.com>
2022-08-11 16:28:06 +0200

    trigger build again

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,11 +2,11 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  
+
   def untested2
     puts `echo lol`
   end
-  
+
   def untested
     puts "one"
     puts "two"
