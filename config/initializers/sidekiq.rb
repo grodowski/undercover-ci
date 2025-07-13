bbe571eb10da6ea196ff15e09209576dda1a0420
Jan Grodowski <jgrodowski@gmail.com>
2025-07-13 15:38:39 +0200

    set redis pool back to 1 for sidekiq_client

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,7 +3,7 @@
 REDIS_SSL_PARAMS = {ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}}.freeze
 
 Sidekiq.configure_client do |config|
-  config.redis = REDIS_SSL_PARAMS
+  config.redis = REDIS_SSL_PARAMS.merge(size: 1)
 end
 
 Sidekiq.configure_server do |config|
