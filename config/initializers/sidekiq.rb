07689d19eb26e1fbf4b97105ed321c32d546d92d
Jan Grodowski <jgrodowski@gmail.com>
2025-09-17 08:47:00 +0200

    Drop concurrency change initializer
    could be loading sidekiq in rake task and creating unnecessary redis connections

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -9,7 +9,3 @@ end
 Sidekiq.configure_server do |config|
   config.redis = REDIS_SSL_PARAMS
 end
-
-# runner jobs are time consuming, always leave an extra slot in the sidekiq concurrency pool
-SIDEKIQ_CONCURRENCY = ENV.fetch("SIDEKIQ_CONCURRENCY", 4).to_i
-Sidekiq::Queue["runner"].limit = SIDEKIQ_CONCURRENCY - 1
