ceda3f93e7ba003616bd59406db5d5f91b2455de
Jan Grodowski <jgrodowski@gmail.com>
2026-03-15 11:09:08 +0100

    new-user-mailer

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,21 @@
 require "rails_helper"
 
 describe User, type: :model do
+  before { stub_const("ENV", ENV.to_h.merge("ADMIN_EMAIL" => "admin@example.com")) }
+
+  it "enqueues a delayed admin notification email on create" do
+    expect do
+      User.create!(uid: "1337", email: "foo@bar.com", token: "tok", name: "Foo Bar")
+    end.to have_enqueued_mail(AdminMailer, :new_user).at(a_value_within(5.seconds).of(2.minutes.from_now))
+  end
+
+  it "does not enqueue admin notification on update" do
+    user = User.create!(uid: "1337", email: "foo@bar.com", token: "tok", name: "Foo Bar")
+    expect do
+      user.update!(name: "New Name")
+    end.not_to have_enqueued_mail(AdminMailer, :new_user)
+  end
+
   it "encrypts the GitHub token" do
     sekritkey = "sekritkey" * 20
     user = User.create!(
